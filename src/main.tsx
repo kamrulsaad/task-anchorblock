5c36599bb166efab0b5b849e17643d0f1003aa9c
Kamrul Saad <skamrul2680@gmail.com>
2023-12-18 22:53:39 +0600

    Created Basic Setup

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,17 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App.tsx'
+import { Toaster } from 'sonner'
 import './index.css'
+import { Provider } from 'react-redux'
+import { RouterProvider } from 'react-router-dom'
+import { store } from './redux/store.ts'
+import router from './routes/index.tsx'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+    <Provider store={store}>
+      <RouterProvider router={router} />
+      <Toaster />
+    </Provider>
+  </React.StrictMode>
 )
